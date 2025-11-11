c1ca28ce70e00d7a6a3d987f4e46bd97d653f64b
lllolll-2001 <vlad.yuresko2001@gmail.com>
2025-11-11 13:15:23 +0200

    Create login.js

---
+++ u.passwordpages/api/login.js
@@ -0,0 +1,18 @@
+const users = [
+  { username: 'boss', password: '1234', role: 'worker' },
+  { username: 'worker', password: '1111', role: 'worker' }
+];
+
+export default function handler(req, res) {
+  if(req.method === 'POST') {
+    const { username, password } = req.body;
+    const user = users.find(u => u.username === username && u.password === password);
+    if(user) {
+      res.status(200).json({ user });
+    } else {
+      res.status(401).json({ message: 'Неверный логин или пароль' });
+    }
+  } else {
+    res.status(405).json({ message: 'Метод не поддерживается' });
+  }
+}
