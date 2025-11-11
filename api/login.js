e33587269f4668544523d9e8fec6d40352b652b2
lllolll-2001 <vlad.yuresko2001@gmail.com>
2025-11-11 13:57:18 +0200

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,25 +1,17 @@
-// pages/api/login.js
-
-// Пользователи
-// Можно расширить список, добавить больше работников
 const users = [
-  { username: 'boss', password: '1234', role: 'boss' },
-  { username: 'worker', password: '1111', role: 'worker' }
+  { username: 'boss', password: 'Kolovo.123q', role: 'boss' },
+  { username: 'worker', password: 'Kolovo.123q', role: 'worker' }
 ];
 
 export default function handler(req, res) {
   if(req.method === 'POST') {
     const { username, password } = req.body;
-
-    // Находим пользователя
     const user = users.find(u => u.username === username && u.password === password);
-
     if(user) {
       res.status(200).json({ user });
     } else {
       res.status(401).json({ message: 'Неверный логин или пароль' });
     }
-
   } else {
     res.status(405).json({ message: 'Метод не поддерживается' });
   }
